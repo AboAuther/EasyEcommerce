525ff41af47e2f884d9e764bc93fb9c4df910654
xiaobaolrg <ruiguo.li@enlighten.finance>
2022-05-18 18:22:42 +0800

    add:api for seller

--- src/pages/businessman/Components/mock/index.js
+++ src/pages/businessman/Components/mock/index.js
@@ -1,92 +1,92 @@
 export const data = [
-    {
-      day: '2021-4-10',
-      value: 301,
-      category: '本月',
-    },
-    {
-      day: '2021-4-10',
-      value: 290,
-      category: '上个月',
-    },
-    {
-      day: '2021-4-11',
-      value: 500,
-      category: '本月',
-    },
-    {
-      day: '2021-4-11',
-      value: 390,
-      category: '上个月',
-    },
-    {
-      day: '2021-4-12',
-      value: 301,
-      category: '本月',
-    },
-    {
-      day: '2021-4-12',
-      value: 900,
-      category: '上个月',
-    },
-    {
-      day: '2021-4-13',
-      value: 302,
-      category: '本月',
-    },
-    {
-      day: '2021-4-13',
-      value: 420,
-      category: '上个月',
-    },
-    {
-      day: '2021-4-14',
-      value: 804,
-      category: '本月',
-    },
-    {
-      day: '2021-4-14',
-      value: 500,
-      category: '上个月',
-    },
-    {
-      day: '2021-4-15',
-      value: 700,
-      category: '本月',
-    },
-    {
-      day: '2021-4-15',
-      value: 520,
-      category: '上个月',
-    },
-    {
-      day: '2021-4-16',
-      value: 631,
-      category: '本月',
-    },
-    {
-      day: '2021-4-16',
-      value: 630,
-      category: '上个月',
-    },
-    {
-      day: '2021-4-17',
-      value: 1004,
-      category: '本月',
-    },
-    {
-      day: '2021-4-17',
-      value: 530,
-      category: '上个月',
-    },
-    {
-      day: '2021-4-18',
-      value: 940,
-      category: '本月',
-    },
-    {
-      day: '2021-4-18',
-      value: 671,
-      category: '上个月',
-    },
-]
+  {
+    day: '2021-4-10',
+    value: 301,
+    category: '本月',
+  },
+  {
+    day: '2021-4-10',
+    value: 290,
+    category: '上个月',
+  },
+  {
+    day: '2021-4-11',
+    value: 500,
+    category: '本月',
+  },
+  {
+    day: '2021-4-11',
+    value: 390,
+    category: '上个月',
+  },
+  {
+    day: '2021-4-12',
+    value: 301,
+    category: '本月',
+  },
+  {
+    day: '2021-4-12',
+    value: 900,
+    category: '上个月',
+  },
+  {
+    day: '2021-4-13',
+    value: 302,
+    category: '本月',
+  },
+  {
+    day: '2021-4-13',
+    value: 420,
+    category: '上个月',
+  },
+  {
+    day: '2021-4-14',
+    value: 804,
+    category: '本月',
+  },
+  {
+    day: '2021-4-14',
+    value: 500,
+    category: '上个月',
+  },
+  {
+    day: '2021-4-15',
+    value: 700,
+    category: '本月',
+  },
+  {
+    day: '2021-4-15',
+    value: 520,
+    category: '上个月',
+  },
+  {
+    day: '2021-4-16',
+    value: 631,
+    category: '本月',
+  },
+  {
+    day: '2021-4-16',
+    value: 630,
+    category: '上个月',
+  },
+  {
+    day: '2021-4-17',
+    value: 1004,
+    category: '本月',
+  },
+  {
+    day: '2021-4-17',
+    value: 530,
+    category: '上个月',
+  },
+  {
+    day: '2021-4-18',
+    value: 940,
+    category: '本月',
+  },
+  {
+    day: '2021-4-18',
+    value: 671,
+    category: '上个月',
+  },
+];
